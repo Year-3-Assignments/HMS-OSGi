017828192601376a1ff3f0926a9110f6fc31165c
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-04-17 12:14:09 +0530

    Add database queries to Doctor Publisher bundle

--- DoctorPublisher/src/doctorpublisher/DoctorServiceImpl.java
+++ DoctorPublisher/src/doctorpublisher/DoctorServiceImpl.java
@@ -1,92 +1,89 @@
 package doctorpublisher;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import hms_db.Database;
+import hms_db.DatabaseImpl;
 
 public class DoctorServiceImpl implements DoctorService {
-	private ArrayList<Doctor> doctors = new ArrayList<>();
-	private ArrayList<Doctor> channeledDoctors = new ArrayList<>();
+	private Connection connection = null;
+	private Statement statement = null;
+	private Database database;
+	private ResultSet resultSet;
+	
+	public DoctorServiceImpl() {
+		database = new DatabaseImpl();
+		connection = database.getDatabaseConnection();
+	}
 
 	@Override
-	public void getAvailableDoctors() {
-		System.out.println("==== Available Doctors ====");
-		for (Doctor doctor: doctors) {
-			if (doctor.isAvailable()) {
-				System.out.println("Doctor ID - " + doctor.getDoctorId());
-				System.out.println("Doctor Name - Dr." + doctor.getFirstName() + " " + doctor.getLastName());
-				System.out.println("Phone Number - " + doctor.getPhoneNumber());
-			}
+	public ResultSet getAvailableDoctors() {
+		String sqlQuery = "SELECT * FROM doctors WHERE availability = 1";
+		
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sqlQuery);
+		} catch (SQLException exc) {
+			System.out.println("Issue with getting available doctor details !!!");
+			System.out.println(exc.getMessage());
+		} finally {
+			return resultSet;
 		}
 	}
 
 	@Override
-	public void getAllDoctors() {
-		System.out.println("==== Doctor List ====");
-		for (Doctor doctor: doctors) {
-			System.out.println("Doctor ID - " + doctor.getDoctorId());
-			System.out.println("Doctor Name - Dr." + doctor.getFirstName() + " " + doctor.getLastName());
-			System.out.println("Phone Number - " + doctor.getPhoneNumber());
+	public ResultSet getAllDoctors() {
+		String sqlQuery = "SELECT * FROM doctors";
+		
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sqlQuery);
+		} catch (SQLException exc) {
+			System.out.println("Issue with getting doctor details !!!");
+			System.out.println(exc.getMessage());
+		} finally {
+			return resultSet;
 		}
 	}
 
 	@Override
-	public void searchDoctorDetails(Integer doctorId) {
-		for (Doctor doctor: doctors) {
-			if (doctor.getDoctorId().equals(doctorId)) {
-				System.out.println("Doctor ID - " + doctor.getDoctorId());
-				System.out.println("Doctor Name - Dr." + doctor.getFirstName() + " " + doctor.getLastName());
-				System.out.println("Phone Number - " + doctor.getPhoneNumber());
-				System.out.println("Availability - " + doctor.isAvailable());
-			}
+	public ResultSet searchDoctorDetails(Integer doctorId) {
+		String sqlQuery = "SELECT * FROM doctors WHERE id = '"+ doctorId +"'";
+		
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sqlQuery);
+		} catch (SQLException exc) {
+			System.out.println("Issue with getting doctor details !!!");
+			System.out.println(exc.getMessage());
+		} finally {
+			return resultSet;
 		}
 	}
 
 	@Override
 	public void channelDoctor(Integer doctorId) {
-		for (Doctor doctor: doctors) {
-			if(doctor.getDoctorId().equals(doctorId)) {
-				doctor.setAvailable(false);
-				channeledDoctors.add(doctor);
-			}
-		}
+		String sqlQuery1 = "UPDATE doctors SET availability = '"+ 0 +"' WHERE id = '"+ doctorId +"'";
+		// Need to implement patient management bundle to implement rest of this function
 	}
 
 	@Override
-	public void createDoctor() {
-		Integer ID, availability;
-		String firstName, lastName, phoneNumber, specialization;
-		boolean available;
-		Scanner scan = new Scanner(System.in);
+	public void insertDoctorDetails(Doctor doctor) {
+		String sqlQuery = "INSERT INTO doctors(first_name, last_name, phone_number, specialization, availability) "
+				+ "VALUES('"+ doctor.getFirstName() +"', '"+ doctor.getLastName() +"', '"+ doctor.getPhoneNumber() +"', "
+						+ "'"+ doctor.getSpeciality() +"', '"+ doctor.isAvailable()+ "')";
 		
-		System.out.println("==== Add a New Doctor ====");
-		System.out.print("Enter Doctor ID - ");
-		ID = scan.nextInt();
-		scan.nextLine();
-		System.out.print("Enter first name - ");
-		firstName = scan.nextLine();
-		System.out.print("Enter last name - ");
-		lastName = scan.nextLine();
-		System.out.println("Enter Specialization - ");
-		specialization = scan.nextLine();
-		System.out.print("Enter phone number - ");
-		phoneNumber = scan.nextLine();
-		System.out.print("Enter availability (1 = Yes, 0 = No) - ");
-		availability = scan.nextInt();
-		
-		switch (availability) {
-			case 1:
-				available = true;
-				break;
-			case 0:
-				available = false;
-				break;
-			default:
-				available = false;
-				break;
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sqlQuery);
+			System.out.println("Doctor details successfully inserted ...");
+		} catch (SQLException exc) {
+			System.out.println("Issue with inserting doctor details !!!");
+			System.out.println(exc.getMessage());
 		}
-		
-		Doctor doctor = new Doctor(ID, firstName, lastName, specialization, phoneNumber, available);
-		doctors.add(doctor);
 	}
 
 }
