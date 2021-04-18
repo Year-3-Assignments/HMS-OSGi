f4813bfb67c3d8b2656f75782f5a096d0dc83998
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-04-18 21:39:19 +0530

    Modify the report management implementation

--- ReportManagement/src/reportmanagement/ReportService.java
+++ ReportManagement/src/reportmanagement/ReportService.java
@@ -1,12 +1,8 @@
 package reportmanagement;
-import java.sql.ResultSet;
 
 public interface ReportService {
 	
-	public void createReport(Report reports);
-	public ResultSet getAllReport();
-	public ResultSet searchReport(Integer reportID);
-	public void requestReport(Integer reportID);
-
-
+	public void createReport();
+	public void getAllReport();
+	public void searchReport();
 }
