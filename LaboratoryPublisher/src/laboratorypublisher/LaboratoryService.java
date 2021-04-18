03dce50a98cce1e333fbbbc8b5cd2075f66b3746
RaveenaWickramasinghe <raveenawickramasinghe@gmail.com>
2021-04-18 23:40:44 +0530

    Initial commit to Laboratory Publisher

---
+++ LaboratoryPublisher/src/laboratorypublisher/LaboratoryService.java
@@ -0,0 +1,10 @@
+package laboratorypublisher;
+
+public interface LaboratoryService {
+	
+	public void createTest();
+	public void getAllTests();
+	public void getTestById();
+	
+
+}
---
+++ LaboratoryPublisher/src/laboratorypublisher/LaboratoryServiceImpl.java
@@ -0,0 +1,109 @@
+package laboratorypublisher;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import hms_db.Database;
+import hms_db.DatabaseImpl;
+import java.util.Scanner;
+
+
+public class LaboratoryServiceImpl implements LaboratoryService{
+	
+	private Connection connection;
+	private Statement statement;
+	private ResultSet resultSet;
+	private Database database;
+	
+	public LaboratoryServiceImpl () {
+		database = new DatabaseImpl();
+		connection = database.getDatabaseConnection();
+	}
+
+	Scanner scanner = new Scanner(System.in);
+	@Override
+	public void createTest() {
+		// TODO Auto-generated method stub
+
+		System.out.println("Enter the Test Name: ");
+		String testName = scanner.nextLine();
+		scanner.nextLine();
+		
+		System.out.println("Enter the Patient ID: ");
+		String patientId = scanner.nextLine();
+		
+		System.out.println("Description : ");
+		String description = scanner.nextLine();
+		
+		System.out.println("Price : ");
+		double price = scanner.nextDouble();
+		
+		String sqlQuery = "INSERT INTO laboratory(test_name, patientId, description, price) VALUES('"+ testName+"', '"+ patientId +"', '"+ description +"', '"+ price +"')";
+		
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sqlQuery);
+			System.out.println("Laboratory tests information successfully inserted ...");
+			
+		} catch (SQLException exc) {
+			System.out.println("Issue in inserting laboratory details !!!");
+			System.out.println(exc.getMessage());
+		}
+	}
+
+	@Override
+	public void getAllTests() {
+		// TODO Auto-generated method stub
+		String sqlQuery = "SELECT * FROM laboratory";
+		
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sqlQuery);
+			System.out.println("==== Lab Results ====");
+			System.out.println(String.format("%-10s %-25s %-20s %-20s", "Lab ID , Test Name,Patient ID, Description , Price, Time"));
+			while (resultSet.next()) {
+				String result = String.format("%-10s %-25s %-20s %-20s", resultSet.getInt("id"), " " + resultSet.getString("test_name") + 
+						" " + resultSet.getInt("patientId"), resultSet.getString("description"),resultSet.getDouble("Price"));
+				System.out.println(result);
+			}
+			
+		} catch (SQLException exc) {
+			System.out.println("There is an issue in getting all the lab tests !!!");
+			System.out.println(exc.getMessage());
+		} 
+	
+		
+	}
+
+	@Override
+	public void getTestById() {
+		// TODO Auto-generated method stub
+		Integer testID;
+		System.out.print("Select a Laboratory ID: ");
+		testID = scanner.nextInt();
+		scanner.nextLine();
+		String sqlQuery = "SELECT * FROM laboratory WHERE id = '"+ testID +"'";
+		
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sqlQuery);
+			System.out.println("==== Lab Results ====");
+			System.out.println(String.format("%-10s %-25s %-20s %-20s", "Lab ID , Test Name,Patient ID, Description , Price, Time"));
+			while (resultSet.next()) {
+				String result = String.format("%-10s %-25s %-20s %-20s", resultSet.getInt("id"), " " + resultSet.getString("test_name") + 
+						" " + resultSet.getInt("patientId"), resultSet.getString("description"),resultSet.getDouble("Price"));
+				System.out.println(result);
+			}
+		} catch (SQLException exc) {
+			System.out.println("There is an issue in getting the tests!!!");
+			System.out.println(exc.getMessage());
+		} 
+	
+	}
+
+	
+	
+
+}
