0d899ed64b79fc5d46e955682fca4c17bce77f5e
parathantl <parathanlive123@gmail.com>
2021-04-18 22:46:27 +0530

    Implementation of Pharmacy Management Publisher and Consumer

--- PharmacyManagement/src/pharmacyconsumer/Activator.java
+++ PharmacyManagement/src/pharmacyconsumer/Activator.java
@@ -18,36 +18,37 @@ public class Activator implements BundleActivator {
 			directorService = context.getServiceReference(PharmacyService.class.getName());
 			
 			PharmacyService service = (PharmacyService) context.getService(directorService); 
-			service.insertPharmacyMedicines();
-//			Scanner scan = new Scanner(System.in);
-//			String option = "1";
-//
-//			do{
-//				
-//				System.out.println("=== Pharmacy Management System ===");
-//				
-//				System.out.println("1. Insert medicine details");
-//				System.out.println("2. Get all medicine details");
-//				System.out.println("3. Insert Prescription details");
-//				System.out.println("4. Get all Prescription details");
-//				System.out.println("5. Get Prescription details by Patient Id");
-//
-//				System.out.print("Enter your choice: ");
-//				option = scan.nextLine();
-//				
-//				if(Integer.parseInt(option) == 1) {
-//					service.insertPharmacyMedicines();
-//				}else if (Integer.parseInt(option) == 2) {
-//					service.getAllMedicineDetails();
-//				}else if (Integer.parseInt(option) == 3) {
-//					service.insertPharmacyPrescription();
-//				}else if (Integer.parseInt(option) == 4) {
-//					service.getAllPrescriptionDetails();
-//				}else if (Integer.parseInt(option) == 5) {
-//					service.getAllPrescriptionDetailsbyId();
-//				}
-//				
-//			}while(Integer.parseInt(option) != 0);	
+
+			Scanner scan = new Scanner(System.in);
+			
+			String option = "1";
+
+			do{
+				
+				System.out.println("=== Pharmacy Management System ===");
+				
+				System.out.println("1. Insert medicine details");
+				System.out.println("2. Get all medicine details");
+				System.out.println("3. Insert Prescription details");
+				System.out.println("4. Get all Prescription details");
+				System.out.println("5. Get Prescription details by Patient Id");
+
+				System.out.print("Enter your choice: ");
+				option = scan.nextLine();
+				
+				if(Integer.parseInt(option) == 1) {
+					service.insertPharmacyMedicines();
+				}else if (Integer.parseInt(option) == 2) {
+					service.getAllMedicineDetails();
+				}else if (Integer.parseInt(option) == 3) {
+					service.insertPharmacyPrescription();
+				}else if (Integer.parseInt(option) == 4) {
+					service.getAllPrescriptionDetails();
+				}else if (Integer.parseInt(option) == 5) {
+					service.getAllPrescriptionDetailsbyId();
+				}
+				
+			}while(Integer.parseInt(option) != 0);	
 			
 	}
 
