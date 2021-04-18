df0b5f433aa92e2b8aa9f752c2d88bd0c17cece4
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-04-18 16:38:22 +0530

    Implement patient consumer bundle

---
+++ PatientManagement/src/patientmanagement/Activator.java
@@ -0,0 +1,33 @@
+package patientmanagement;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+
+import doctorpublisher.DoctorService;
+import patientpublisher.Patient;
+import patientpublisher.PatientPublisher;
+
+public class Activator implements BundleActivator {
+
+	private ServiceReference patientManagementReference;
+	private ServiceReference doctorPublisherReference;
+	
+	public void start(BundleContext context) throws Exception {
+		patientManagementReference = context.getServiceReference(PatientPublisher.class.getName());
+		doctorPublisherReference = context.getServiceReference(DoctorService.class.getName());
+		DoctorService doctorService = (DoctorService) context.getService(doctorPublisherReference);
+		PatientPublisher service = (PatientPublisher) context.getService(patientManagementReference);
+		
+//		service.insertPatientDetails();
+		doctorService.getAvailableDoctors();
+		doctorService.channelDoctor();
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Patient Management Service Stopped!!!");
+		context.ungetService(patientManagementReference);
+	}
+
+}
